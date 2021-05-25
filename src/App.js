68e8e47b33145ae0ea209fc16eccced6a60e3ad5
Mathew Kizhakkadathu <m.kizzy.up@gmail.com>
2021-05-25 10:29:23 -0500

    Git Card sales removed

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
             tmpData += data["Ticket Order Volume"] * 100;
         else if(activeTabKey === 2)
             tmpData += data["Ticket Size"] * 100;
-        else if(activeTabKey === 3) // Gift Card Sales
+        else if(activeTabKey === 3)
             tmpData += flag ? 10000 : data["Gift Card Sales"] * 100;
         else if(activeTabKey === 4) // Gift Card Volume case
             tmpData += data["Gift Card Volume"] * 100;
